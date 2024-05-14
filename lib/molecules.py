b25ad83e65c95ebd4fcbd3e5a307c4fc38fe5a5e
Oliver Rhodes <163131858+OliverZhengRhodes@users.noreply.github.com>
2024-05-14 15:55:37 +0100

    Add files via upload

---
+++ lib/molecules.py
@@ -0,0 +1,11 @@
+molecules = ["Ammonia", "Aspirin", "Calcium Oxide", "Carbon Dioxide",
+             "Cobalt Tetrachloride", "DDT", "Ethanol", "Ethlyene",
+             "Morphine", "Penicilin", "Phosphoric Acid", "Potassium Nitrate",
+             "Progestin", "Sodium Chloride", "Sodium Hydroxide",
+             "Sodium Stearate", "Silicon Dioxide", "Sulfuric Acid", "Toluene", "Urea"]
+molecule_information = {
+    "melting point":"",
+    "boiling point":"",
+    "hazards":"",
+    "uses":""}
+molecule_string = ["[Co-2](Cl)(Cl)(Cl)Cl"]
