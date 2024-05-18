35dd514eb59906fe4ec55e17490a47e86ca97f12
Oliver Rhodes <163131858+OliverZhengRhodes@users.noreply.github.com>
2024-05-19 00:54:07 +0100

    Update molecules.py
    added more text

--- lib/molecules.py
+++ lib/molecules.py
@@ -1,11 +1,14 @@
 molecules = ["Ammonia", "Aspirin", "Calcium Oxide", "Carbon Dioxide",
              "Cobalt Tetrachloride", "DDT", "Ethanol", "Ethlyene",
-             "Morphine", "Penicilin", "Phosphoric Acid", "Potassium Nitrate",
+             "Morphine", "Penicilin G", "Phosphoric Acid", "Potassium Nitrate",
              "Progestin", "Sodium Chloride", "Sodium Hydroxide",
              "Sodium Stearate", "Silicon Dioxide", "Sulfuric Acid", "Toluene", "Urea"]
 molecule_information = {
-    "melting point":"",
-    "boiling point":"",
-    "hazards":"",
-    "uses":""}
+    "introduction":("sdf","sdf","asd"),
+    "melting point":("-77.73"+u"\u2103","136"+u"\u2103","2,572"+u"\u2103"),
+    "boiling point":("3","4","6"),
+    "hazards":("9","8","7"),
+    "uses":("sd","sdf","asd")}
 molecule_string = ["[Co-2](Cl)(Cl)(Cl)Cl"]
+
+molecule_information1 = ("dfsdfsdfsdfs","dsfsfsdfdsfds\nsdfsdfd","hdsksdhdfkhds hj sdhjshds dh\nsdhfhdshj hads\ns sdhhi9")
